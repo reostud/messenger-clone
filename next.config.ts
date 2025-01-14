20f36b90348c8d827a40bea9d40d1631b8db0911
reostud <reostud@gmail.com>
2025-01-14 22:18:54 +0700

    refactor next.config.ts and prisma schema for improved clarity and structure

--- next.config.ts
+++ next.config.ts
@@ -1,42 +1,17 @@
 import type { NextConfig } from "next";
 
-//const nextConfig: NextConfig = {
-/* config options here */
-//swcPlugins: [["next-superjson-plugin", {}]],
-//images: {
-//domains: [
-//"res.cloudinary.com",
-//"avatars.githubusercontent.com",
-//"lh3.googleusercontent.com",
-//],
-//},
-//};
-
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "res.cloudinary.com",
-      },
-      {
-        protocol: "https",
-        hostname: "avatars.githubusercontent.com",
-      },
-      {
-        protocol: "https",
-        hostname: "lh3.googleusercontent.com",
-      },
+    domains: [
+      "res.cloudinary.com",
+      "avatars.githubusercontent.com",
+      "lh3.googleusercontent.com",
     ],
   },
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if\
-    // your project has type errors.
-    // !! WARN !!
     ignoreBuildErrors: true,
   },
 };
