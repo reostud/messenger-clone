7598ababe20cf4e4739c1febe96a78ca432ac91a
reostud <reostud@gmail.com>
2025-01-14 16:53:01 +0700

    add eslint configuration to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,35 +1,37 @@
 import type { NextConfig } from "next";
 
 //const nextConfig: NextConfig = {
-  /* config options here */
-  //swcPlugins: [["next-superjson-plugin", {}]],
-  //images: {
-    //domains: [
-      //"res.cloudinary.com",
-      //"avatars.githubusercontent.com",
-      //"lh3.googleusercontent.com",
-    //],
-  //},
+/* config options here */
+//swcPlugins: [["next-superjson-plugin", {}]],
+//images: {
+//domains: [
+//"res.cloudinary.com",
+//"avatars.githubusercontent.com",
+//"lh3.googleusercontent.com",
+//],
+//},
 //};
 
-const nextConfig: NextConfig = { 
-  
-  images: { 
-    remotePatterns: [ 
-      { 
-        protocol: 'https', 
-        hostname: 'res.cloudinary.com', 
-      }, 
-      { 
-        protocol: 'https', 
-        hostname: 'avatars.githubusercontent.com', 
-      }, 
-      { 
-        protocol: 'https', 
-        hostname: 'lh3.googleusercontent.com', 
-      }, 
-    ], 
-  }, 
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+    ],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
